fb6513f182e97127f10861d008f706e18bce2d6a
Adarsh Vijayaraghavan <adarshvijayaraghavan@gmail.com>
2018-04-22 05:09:26 -0400

    Starting to get a working code

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
-
+var cors = require('cors');
+app.use(cors({origin: 'http://localhost:8888'}));
 router.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
 });
 
 router.get("/",function(req,res){
+  
   res.sendFile(path + "index.html");
 });
 
